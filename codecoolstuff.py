fac384049e2573eaf1c69ff709c6bbadf2d2b252
Péter Kőrössy <meanwhile@vipmail.hu>
2017-05-07 18:56:36 +0200

    First question done

--- codecoolstuff.py
+++ codecoolstuff.py
@@ -1,15 +1,24 @@
 import psycopg2
 
-try:
-    conn = psycopg2.connect("dbname='peter' user='peter' host='localhost' password='korpeter'")
-    conn.autocommit = True
-    cur = conn.cursor()
 
-except Exception as e:
-    print("Can't connect. Invalid dbname, user or password.")
-    print(e)
+def first(cur):
+    cur.execute("""SELECT first_name, last_name FROM mentors;""")
+    rows = cur.fetchall()
+    for element in rows:
+        print(element[0] + " " + element[1])
 
-cur.execute("""SELECT first_name, last_name FROM mentors;""")
-rows = cur.fetchall()
-for element in rows:
-    print(element)
+
+def main():
+    try:
+        conn = psycopg2.connect("dbname='peter' user='peter' host='localhost' password='korpeter'")
+        conn.autocommit = True
+        cur = conn.cursor()
+
+    except Exception as e:
+        print("Can't connect. Invalid dbname, user or password.")
+        print(e)
+
+    first(cur)
+
+if __name__ == '__main__':
+    main()
